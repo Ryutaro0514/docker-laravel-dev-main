b7bba467df3e2dd43f94d64c4b15c6c6ae7455be
Ryutaro0514 <ryuryu_758_3602@icloud.com>
2025-09-02 16:36:49 +0900

    update

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("/todos",[ApiController::class,"getApi"])->name("getApi");
 
+
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
